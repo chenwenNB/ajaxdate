56ccff3b42334f8453613d29fabe600a2faeb1b5
chenwenNB <1791132064@qq.com>
2019-10-17 21:48:11 +0800

    项目测试

---
+++ xjWX/js/remStyle.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Administrator on 2019/10/16.
+ */
+{
+    let wid = document.documentElement.clientWidth||document.body.innerWidth;
+    document.documentElement.style.fontSize = wid/750*100+"px";
+}
